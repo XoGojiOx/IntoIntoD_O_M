80e61562937e43216e198b9181ea7142447fcd91
Richard Sanders <xogojiox@gmail.com>
2024-05-20 09:39:38 -0500

    Changes to be committed: 	modified:   Selecting/app.js

--- Selecting/app.js
+++ Selecting/app.js
@@ -2,7 +2,7 @@ const allImages = document.getElementsByTagName("img");
 
 for (let img of allImages) {
   img.src =
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg";
+    "https://cdn.britannica.com/18/137318-050-29F7072E/rooster-Rhode-Island-Red-roosters-chicken-domestication.jpg";
 }
 
 const squareImages = document.getElementsByClassName("square");
@@ -12,8 +12,17 @@ for (let img of squareImages) {
     "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg";
 }
 
-const links = document.querySelectorAll("p a");
+/*
+//This will print out each href for all a's that fall within p (paragraphs)
+// const links = document.querySelectorAll("p a");
 
 // for (let link of links) {
 //     console.log(link.href)
 // }
+*/
+
+//This gives us the first p element.
+// document.squerySelector('p');
+
+/* Selecting by a element selector */
+document.querySelector("a[title='Java']");
